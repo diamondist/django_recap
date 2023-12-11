f319fbd5f0a007baac7da7daf0fe4cb0653eaba0
Dmitrii Blinov <64638976+diamondist@users.noreply.github.com>
2023-12-11 18:51:55 +0700

    changes in models and template

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from projects.models import Project
 
 
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', {'projects': projects})
